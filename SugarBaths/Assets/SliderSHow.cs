90e19dcf5ead7b921dfc373ca1cd6d6dfd444662
Yura <126978770+YuraEz@users.noreply.github.com>
2024-07-28 02:45:16 +0300

    97%

--- SugarBaths/Assets/SliderSHow.cs
+++ SugarBaths/Assets/SliderSHow.cs
@@ -5,38 +5,49 @@ using UnityEngine.UI;
 
 public class SliderSHow : MonoBehaviour
 {
-    public Sprite[] images; // массив картинок для слайдшоу
-    public GameObject[] obj;
-    public Image displayImage; // UI Image для отображения текущего изображения
-    private int currentIndex = 0; // текущий индекс изображения
+    public GameObject[] obj; // массив игровых объектов для слайдшоу
+    public GameObject[] obj2;
+    private int currentIndex = 0; // текущий индекс объекта
 
     [Space]
     public Button left;
     public Button right;
 
-    // Метод для смены изображения на следующее
+    // Метод для смены объекта на следующий
     private void Awake()
     {
-        left.onClick.AddListener(PreviousImage);
-        right.onClick.AddListener(NextImage);
+        left.onClick.AddListener(PreviousObject);
+        right.onClick.AddListener(NextObject);
+
+        // Обеспечим, что только первый объект активен при запуске
+        UpdateObject();
     }
 
-    public void NextImage()
+    public void NextObject()
     {
-        currentIndex = (currentIndex + 1) % images.Length;
-        UpdateImage();
+        currentIndex = (currentIndex + 1) % obj.Length;
+        UpdateObject();
     }
 
-    // Метод для смены изображения на предыдущее
-    public void PreviousImage()
+    // Метод для смены объекта на предыдущий
+    public void PreviousObject()
     {
-        currentIndex = (currentIndex - 1 + images.Length) % images.Length;
-        UpdateImage();
+        currentIndex = (currentIndex - 1 + obj.Length) % obj.Length;
+
+        UpdateObject();
     }
 
-    // Метод для обновления текущего изображения
-    private void UpdateImage()
+    // Метод для обновления текущего объекта
+    private void UpdateObject()
     {
-        displayImage.sprite = images[currentIndex];
+        // Выключаем все объекты
+        for (int i = 0; i < obj.Length; i++)
+        {
+            obj[i].SetActive(false);
+            obj2[i].SetActive(false);
+        }
+        // Включаем текущий объект
+        obj[currentIndex].SetActive(true);
+        obj2[currentIndex].SetActive(true);
     }
 }
